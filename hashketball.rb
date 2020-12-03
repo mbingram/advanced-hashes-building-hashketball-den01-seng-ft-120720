7a31a8a60787df26197744f8b930a01d14d9d076
charming-input-2976 <mbingramm@gmail.com>
2020-12-03 18:33:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,6 +16,16 @@ def game_hash
       :blocks => 1 ,
       :slam_dunks => 1
       }
+      {:player_name => "" ,
+      :number =>  ,
+      :shoe =>  ,
+      :points =>  ,
+      :rebounds =>  ,
+      :assists =>  ,
+      :steals =>  ,
+      :blocks =>  ,
+      :slam_dunks => 
+      }
       ]
     } 
   } 
