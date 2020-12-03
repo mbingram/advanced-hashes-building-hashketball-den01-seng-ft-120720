7b251064a3524b165480d38f85f5a3578c893b2f
charming-input-2976 <mbingramm@gmail.com>
2020-12-03 18:11:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,11 +3,25 @@
 def game_hash {
   :home =>
   {:team_name => "Brooklyn Nets"} ,
-  {:colors => " "} ,
+  {:colors => ["Black" , "White"]} ,
+  {:players => [
+    {:player_name => " "} ,
+    {:number => " "} ,
+    {:shoe => " "} ,
+    {:points => " "} ,
+    {:rebounds => " "} ,
+    {:assists => " "} ,
+    {:steals => " "} ,
+    {:blocks => " "} ,
+    {:slam_dunks => " "}
+    ]
+  } ,
+  
+  
+  :away =>
+  {:team_name => "Charlotte Hornets"} ,
+  {:colors => ["Turquoise" , "Purple"]} ,
   {:players => " "}
-  
-  
-  :away => {:team_name => "Charlotte Hornets"} , {:colors => " "} , {:players => " "}
     
     
   }
