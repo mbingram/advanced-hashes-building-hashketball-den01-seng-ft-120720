7997ed81060622135c654a52749325550209af9d
charming-input-2976 <mbingramm@gmail.com>
2020-12-03 18:04:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,10 @@
+
+
 def game_hash {
   {:home => {:team_name => "Brooklyn Nets"} , {:colors => " "} , {:players => " "}
-  :away => "Charlotte Hornets"}
+  
+  
+  {:away => {:team_name => "Charlotte Hornets"} , {:colors => " "} , {:players => " "}
     
     
   }
